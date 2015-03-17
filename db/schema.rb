e407ea0dcbf0077873b156c4ccb84ae7e068d427
Grace Mekarski and Jake Kaad <gracelauren909@gmail.com>
2015-03-17 14:05:54 -0700

    add bootstrap gem, course model and migrations

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150317170131) do
+ActiveRecord::Schema.define(version: 20150317210009) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "courses", force: :cascade do |t|
+    t.string   "name"
+    t.string   "description"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "lessons", force: :cascade do |t|
     t.string   "title"
